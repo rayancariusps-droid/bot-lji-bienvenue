144b13d024fbdd0f85c61c729ac591d2961de2a3
rayancariusps-droid <rayancariusps@gmail.com>
2025-12-17 14:46:56 +0100

    Create server.js

---
+++ server.js
@@ -0,0 +1,7 @@
+const express = require("express");
+const app = express();
+
+app.get("/", (req, res) => res.send("Bot en ligne 🌸"));
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => console.log(`Serveur ping sur le port ${PORT}`));

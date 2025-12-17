4991cb7ab297aa825e082dd557e0af1d93aea8ee
rayancariusps-droid <rayancariusps@gmail.com>
2025-12-17 16:52:23 +0100

    Update server.js

--- server.js
+++ server.js
@@ -1,7 +1,9 @@
 const express = require("express");
 const app = express();
 
+// Page principale
 app.get("/", (req, res) => res.send("Bot en ligne 🌸"));
 
+// Port
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => console.log(`Serveur ping sur le port ${PORT}`));

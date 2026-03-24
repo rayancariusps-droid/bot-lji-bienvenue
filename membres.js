caa91bf70e5306cf99d5e1383f7b0d8011f0ec7f
rayancariusps-droid <rayancariusps@gmail.com>
2026-03-24 19:23:26 +0100

    Update membres.js

--- membres.js
+++ membres.js
@@ -1,1 +1,11 @@
+const { SlashCommandBuilder } = require("discord.js");
 
+module.exports = {
+    data: new SlashCommandBuilder()
+        .setName("membres")
+        .setDescription("Voir le nombre de membres"),
+
+    async execute(interaction) {
+        await interaction.reply(`👥 ${interaction.guild.memberCount} membres`);
+    }
+};

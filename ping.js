96f004d64b9bab6c21ee0aa695f39999171dcdf9
rayancariusps-droid <rayancariusps@gmail.com>
2026-03-24 19:22:45 +0100

    Update ping.js

--- ping.js
+++ ping.js
@@ -1,1 +1,11 @@
+const { SlashCommandBuilder } = require("discord.js");
 
+module.exports = {
+    data: new SlashCommandBuilder()
+        .setName("ping")
+        .setDescription("Ping du bot"),
+
+    async execute(interaction) {
+        await interaction.reply(`🏓 Pong !`);
+    }
+};
